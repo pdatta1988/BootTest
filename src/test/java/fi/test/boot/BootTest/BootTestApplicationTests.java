f8c56a30884e3601cac7b886f4f3234053383fe5
Pathikrit Datta <1380805@tcs.com>
2024-08-08 13:02:30 +0300

    Structure loaded

---
+++ src/test/java/fi/test/boot/BootTest/BootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.test.boot.BootTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
